ab53c2bea97dd01fa4f49ce5697360a247c02dcb
SushmithaMuruga96 <s30496usi@gmail.com>
2024-09-14 17:33:59 +0400

    React Router initial set up

---
+++ react-router-demo/src/App.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import { Home } from "./Components/Home";
+// import { About } from "./Components/About";
+import { Navbar } from "./Components/Navbar";
+import { OrderSummary } from "./Components/OrderSummary";
+import { NoMatch } from "./Components/NoMatch";
+import { Products } from "./Components/Products";
+import { FeaturedProduct } from "./Components/FeaturedProduct";
+import { NewProduct } from "./Components/NewProduct";
+import { User } from "./Components/User";
+import { UserDetails } from "./Components/UserDetails";
+import { Admin } from "./Components/Admin";
+const LazyAbout = React.lazy(() => import("./Components/About"));
+
+function App() {
+  //index Route will render component at parent route
+  //userId can be a ID or string if it is string Router priortize the match router
+  // else users/admin display UserDetails component when there is no admin comp
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route
+          path="about"
+          element={
+            <React.Suspense fallback="Loading...">
+              <LazyAbout />
+            </React.Suspense>
+          }
+        />
+        <Route path="order-summary" element={<OrderSummary />} />
+        <Route path="*" element={<NoMatch />} />
+        <Route path="products" element={<Products />}>
+          <Route index element={<FeaturedProduct />} />
+          <Route path="feature" element={<FeaturedProduct />} />
+          <Route path="new" element={<NewProduct />} />
+        </Route>
+        <Route path="users" element={<User />}>
+          <Route path=":userId" element={<UserDetails />} />
+          <Route path="admin" element={<Admin />} />
+        </Route>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
